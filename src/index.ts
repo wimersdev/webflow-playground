2b59acb4e99286254bf192961e37f0316a8c7333
wimersdev <119748574+wimersdev@users.noreply.github.com>
2023-11-08 11:01:54 +0200

    Updated moving squares

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 //* <script defer src="http://localhost:3000/index.js"></script> *//
 
 import squares from "$utils/hero-squares";
+import graph_hiw from "$utils/graph_hiw";
 
 
-squares()
+graph_hiw()
 
 
 
